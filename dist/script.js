e6db6b968b8d87d8ff8c56dd7f973a4ba699c6d1
6tg78 <romek.gardys@gmail.com>
2022-01-18 23:06:23 +0100

    search working

--- dist/script.js
+++ dist/script.js
@@ -1,10 +1,12 @@
 const API_URL =
     'https://api.themoviedb.org/3/movie/top_rated?api_key=70f7b27b0933c73923f41cdc217909fc&page=1';
 const IMG_PATH = 'https://image.tmdb.org/t/p/w512/';
-const SEARCH_URL =
+const SEARCH_API =
     'https://api.themoviedb.org/3/search/movie?api_key=70f7b27b0933c73923f41cdc217909fc&query="';
 
-    const form = document
+const form = document.getElementById('form');
+const search = document.getElementById('search');
+
 //get movies
 getTitles(API_URL);
 
@@ -14,3 +16,15 @@ async function getTitles(url) {
 
     console.log(data.results);
 }
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const movieName = search.value;
+
+    if (movieName && movieName !== '') {
+        getTitles(SEARCH_API + movieName);
+        search.value = '';
+    } else {
+        window.location.reload();
+    }
+});
