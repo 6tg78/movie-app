8e0aabfd69f0066f8260f08b1706ee0e0a93ab01
Kacper Cieciński <ciecinski.kacper@gmail.com>
2022-01-19 00:31:15 +0100

    fix showing cards

--- dist/script.js
+++ dist/script.js
@@ -4,29 +4,28 @@ const IMG_PATH = 'https://image.tmdb.org/t/p/w300';
 const SEARCH_API =
     'https://api.themoviedb.org/3/search/movie?api_key=70f7b27b0933c73923f41cdc217909fc&query="';
 
-const form = document.getElementById('form');
-const search = document.getElementById('search');
-const main = document.getElementById('main');
 
-//get movies
-getTitles(API_URL);
+window.addEventListener('DOMContentLoaded', () => {
+    const form = document.getElementById('form');
+    const search = document.getElementById('search');
+    const main = document.getElementById('main');
 
-async function getTitles(url) {
-    const res = await fetch(url);
-    const data = await res.json();
+    async function getTitles(url) {
+        const res = await fetch(url);
+        const data = await res.json();
 
-    console.log(data.results);
-}
+        return data.results;
+    }
 
-function showMovies(movies) {
-    main.innerHTML = '';
-    movies.forEach((movie) => {
-        const { title, poster_path, vote_average, overview } = movie;
+    function showMovies(movies) {
+        main.innerHTML = '';
+        movies.forEach((movie) => {
+            const { title, poster_path, vote_average, overview } = movie;
 
-        const movieElem = document.createElement('div');
-        movieElem.classList.add('movie-container');
+            const movieElem = document.createElement('div');
+            movieElem.classList.add('movie-container');
 
-        movieElem.innerHTML = `            
+            movieElem.innerHTML = `            
         <div class="movie-container">
             <img
             src="${IMG_PATH + poster_path}"
@@ -35,8 +34,8 @@ function showMovies(movies) {
             <div class="info">
                 <h3>${title}</h3>
                 <span class="${getRatingColor(
-                    vote_average
-                )}">${vote_average}</span>
+                vote_average
+            )}">${vote_average}</span>
             </div>
             <div class="description">
             <h3>Description</h3>
@@ -44,30 +43,36 @@ function showMovies(movies) {
             </div>
         </div>`;
 
-        main.appendChild(movieElem);
-        console.log(movie);
+            main.appendChild(movieElem);
+            console.log(movie);
+        });
+    }
+
+    function getRatingColor(vote) {
+        if (vote >= 8.1) {
+            return 'green';
+        } else if (vote >= 4.5) {
+            return 'yellow';
+        } else {
+            return 'red';
+        }
+    }
+
+    //event listener in form
+    form.addEventListener('submit', (e) => {
+        e.preventDefault();
+        const movieName = search.value;
+
+        if (movieName && movieName !== '') {
+            getTitles(SEARCH_API + movieName).then((movies) => showMovies(movies))
+            search.value = '';
+        } else {
+            window.location.reload();
+        }
     });
-}
 
-function getRatingColor(vote) {
-    if (vote >= 8.1) {
-        return 'green';
-    } else if (vote >= 4.5) {
-        return 'yellow';
-    } else {
-        return 'red';
-    }
-}
 
-//event listener in form
-form.addEventListener('submit', (e) => {
-    e.preventDefault();
-    const movieName = search.value;
-
-    if (movieName && movieName !== '') {
-        getTitles(SEARCH_API + movieName);
-        search.value = '';
-    } else {
-        window.location.reload();
-    }
+    getTitles(API_URL).then((movies) => showMovies(movies))
+    console.log({ movies }, 'a')
+    showMovies(movies)
 });
